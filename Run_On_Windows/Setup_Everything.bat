888df80703659b0edeb172ee21f9ddcbd74c5c21
Olly <56711484+Wologman@users.noreply.github.com>
2024-03-01 15:57:17 +1300

    Add files via upload

--- Run_On_Windows/Setup_Everything.bat
+++ Run_On_Windows/Setup_Everything.bat
@@ -1,5 +1,5 @@
 @echo off
 cd ..\Python\Setup
 powershell -ExecutionPolicy Bypass -Command "conda init powershell"
-powershell -ExecutionPolicy Bypass -NoProfile -File "MegaDetector_Setup.ps1"
+powershell -ExecutionPolicy Bypass  -File "MegaDetector_Setup.ps1"
 powershell -ExecutionPolicy Bypass -NoExit -File "Classifier_Setup.ps1"
